8baf5835579dd341e278a3f1d841bc3f88ea287c
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-01-12 19:40:08 +0100

    fix path click

--- src/components/map/ClickCallback.tsx
+++ src/components/map/ClickCallback.tsx
@@ -5,6 +5,10 @@ import { useContext } from "react";
 import { useMapEvents } from "react-leaflet";
 import { StateContext } from "./StateManager";
 
+const MODE_TRANSITIONS = {
+  [Mode.SelectPath]: Mode.SelectPath,
+} as Record<number, Mode>;
+
 export function ClickCallback({
   callback: setTargetPos,
 }: {
@@ -13,7 +17,10 @@ export function ClickCallback({
   const { setState } = useContext(StateContext);
   useMapEvents({
     click: (e: LeafletMouseEvent) => {
-      setState?.((s) => ({ ...s, mode: Mode.Standard }));
+      setState?.((s) => ({
+        ...s,
+        mode: MODE_TRANSITIONS[s?.mode] ?? Mode.Standard,
+      }));
       setTargetPos([e.latlng.lat, e.latlng.lng]);
     },
   });
